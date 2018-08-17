003cf9c792ad02058c7ccb463ecc71a955d7f2f4
Noogic <noogic@gmail.com>
2018-08-17 19:32:55 +0200

    Implement Login component and create Home component

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -1,6 +1,12 @@
 import Login from '@/stories/auth/Login';
+import Home from '@/stories/Home';
 
 export default [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
     {
         path: '/login',
         name: 'login',
---
+++ resources/assets/js/stories/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p v-show="token">api_token: {{ token }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                token: null
+            }
+        },
+        created() {
+            this.token = localStorage.getItem('api_token');
+        }
+    }
+</script>
