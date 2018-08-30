0466002e14652095c841da16f00dce5126691116
franinowa <franinowa@gmail.com>
2018-08-30 14:21:29 +0200

    Encapsulate navigation component

--- resources/assets/js/stories/Home.vue
+++ resources/assets/js/stories/Home.vue
@@ -1,42 +1,6 @@
 <template>
     <div class="w-full h-full bg-grey-lightest">
-        <nav class="bg-grey-darkest">
-            <div class="container mx-auto px-4 py-2 flex items-center justify-between flex-wrap ">
-                <div class="flex items-center flex-no-shrink text-white mr-6">
-                    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                    <span class="font-semibold text-xl tracking-tight">LevelApp</span>
-                </div>
-                <div class="block lg:hidden">
-                    <button class="flex items-center px-3 py-2 border rounded text-grey-lighter border-grey-light hover:text-white hover:border-white">
-                        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                    </button>
-                </div>
-                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                    <div class="text-sm lg:flex-grow">
-                        <router-link
-                            :to="{ name: 'home' }"
-                            tag="button"
-                            class="no-underline block mt-4 lg:inline-block lg:mt-0 text-grey-lighter hover:text-white mr-4">
-                            Home
-                        </router-link>
-                        <router-link
-                            :to="{ name: 'me' }"
-                            tag="button"
-                            class="no-underline block mt-4 lg:inline-block lg:mt-0 text-grey-lighter hover:text-white mr-4">
-                            User
-                        </router-link>
-                    </div>
-                    <div>
-                        <router-link
-                            :to="{ name: 'login' }"
-                            tag="button"
-                            class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">
-                            Login
-                        </router-link>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        <Navigation />
 
         <div class="container mx-auto px-4 py-2 overflow-y-scroll">
             <div v-if="token">
@@ -48,16 +12,17 @@
 </template>
 
 <script>
+    import Navigation from '@/layout/Navigation';
     import api from '@/api';
 
     export default {
         name: 'Home',
+        components: { Navigation },
         methods: {
             logout() {
                 api.auth.logout();
             }
         },
-
         computed: {
             token () {
                 return api.auth.token;
--- resources/assets/js/stories/auth/Login.vue
+++ resources/assets/js/stories/auth/Login.vue
@@ -1,45 +1,51 @@
 <template>
-    <div class="flex justify-center items-center w-full h-full bg-grey-lightest">
-        <div class="w-full max-w-xs">
-            <form
-                class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                @submit.prevent="sendForm">
+    <div class="flex flex-col w-full h-full bg-grey-lightest">
+        <Navigation />
 
-                <div class="mb-4">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
-                    <input v-model="form.email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
-                </div>
-                <div class="mb-6">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input v-model="form.password" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********">
-                    <!-- <p class="text-red text-xs italic">Please choose a password.</p> -->
-                </div>
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Sign In
-                    </button>
-                    <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
-                        Forgot Password?
-                    </a>
-                </div>
-            </form>
-            <p class="text-center text-grey text-xs">
-                ©2018 LevelApp Corp. All rights reserved.
-            </p>
+        <div class="flex justify-center items-center flex-1">
+            <div class="w-full max-w-xs">
+                <form
+                    class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                    @submit.prevent="sendForm">
+
+                    <div class="mb-4">
+                        <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
+                            Email
+                        </label>
+                        <input v-model="form.email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
+                    </div>
+                    <div class="mb-6">
+                        <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+                            Password
+                        </label>
+                        <input v-model="form.password" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********">
+                        <!-- <p class="text-red text-xs italic">Please choose a password.</p> -->
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                            Sign In
+                        </button>
+                        <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+                <p class="text-center text-grey text-xs">
+                    ©2018 LevelApp Corp. All rights reserved.
+                </p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Navigation from '@/layout/Navigation';
     import api from '@/api';
     import store from '@/store';
 
     export default {
         name: "Login",
+        components: { Navigation },
         data() {
             return {
                 form: {
